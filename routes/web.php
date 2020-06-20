2c268df7da2702bcbf264255d03351630e24f504
Henry Lee <henryleeworld@gmail.com>
2020-06-21 00:14:20 +0800

    feat:初始化

---
+++ routes/web.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::group(['middleware' => 'fw-block-blacklisted'], function () 
+{
+    Route::get('coming-soon', function()
+    {
+        return view('coming-soon');
+    });
+
+    Route::group(['middleware' => 'fw-only-whitelisted'], function () 
+    {
+        Route::get('/', 'HomeController@index');
+    });
+});
